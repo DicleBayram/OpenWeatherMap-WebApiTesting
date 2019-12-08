135451ad065c3f56895b92572ebad037908b9022
Dicle Bayram <47928885+DicleBayram@users.noreply.github.com>
2019-12-08 21:00:08 +0300

    Add files via upload

---
+++ ApiResponse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Stylelabs.MQA.WebApiTesting.Tests
+{
+    public class ApiResponse
+    {
+        private const string ApiUrlString = "http://api.openweathermap.org/data/2.5/weather?q=";
+        private string ApiUrlStringWithKey;
+
+        public string GetResponse(string city)
+        {
+            StringBuilder responseString = new StringBuilder();
+            ApiUrlStringWithKey = String.Format(ApiUrlString + "{0}", city);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ApiUrlString);
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            try
+            {
+                Stream receiveStream = response.GetResponseStream();
+
+                if (receiveStream != null)
+                {
+                    StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                    string line = "";
+                    while ((line = readStream.ReadLine()) != null)
+                    {
+                        responseString.Append(line);
+                    }
+                    response.Close();
+                    readStream.Close();
+                }
+            }
+            catch (Exception)
+            {
+                responseString.Append("Can't get response");
+
+            }
+            return responseString.ToString();
+        }
+    }
+}
---
+++ WeatherMap.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace Stylelabs.MQA.WebApiTesting.Tests
+{
+    public class WeatherMap
+    {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _webDriver = new ChromeDriver(@"C:\Users\dicleba");
+        }
+        [Test]
+        public void TestCity()
+        {
+            const string cityString = "New York";
+            ApiResponse apiResponse = new ApiResponse();
+            string expectedResult = apiResponse.GetResponse(cityString);
+            Thread.Sleep(1000);
+
+            //string displayedResponse = responseElement.Text;
+            //Assert.IsTrue(expectedResult.max_temperature > 10degree);
+        }
+
+       [TearDown]
+        public void Teardown()
+        {
+            _webDriver.Close();
+            _webDriver.Quit();
+        }
+    }
+}
